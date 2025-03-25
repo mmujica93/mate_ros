4f140e90ac7f9e3bc78d79ed1d801d9a758e3114
mmujica93 <martin.mujica@laas.fr>
2025-03-25 20:06:47 +0100

    Delete mate_ros directory

--- mate_ros/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='mate_ros',  # Replace with your package name
-    version='0.0.0',
-    packages=find_packages(include=['resources', 'resources.*']),
-    install_requires=[],
-    package_dir={'': '.'},  # The root is the base directory
-)
